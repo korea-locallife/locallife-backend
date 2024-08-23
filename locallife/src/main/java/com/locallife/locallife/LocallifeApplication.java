9e53e717f9d2a9069feee8dc40d2e32d4c2918ac
JJimini <min102602@pusan.ac.kr>
2024-08-23 23:06:17 +0900

    init: 프로젝트 초기 설정

---
+++ locallife/src/main/java/com/locallife/locallife/LocallifeApplication.java
@@ -0,0 +1,13 @@
+package com.locallife.locallife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocallifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocallifeApplication.class, args);
+	}
+
+}
---
+++ locallife/src/test/java/com/locallife/locallife/LocallifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locallife.locallife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocallifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
