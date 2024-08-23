6af16b2319273f4e105b998f6bc8082cbcb33446
JJimini <min102602@pusan.ac.kr>
2024-08-24 07:02:11 +0900

    feat: member entity 생성

--- locallife/src/main/java/com/locallife/locallife/member/entity/Member.java
+++ locallife/src/main/java/com/locallife/locallife/member/entity/Member.java
@@ -1,13 +1,54 @@
 package com.locallife.locallife.member.entity;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-//@Entity
-//@Getter
-//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String memberId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column
+    private String email;
+
+    @Column(nullable = false)
+    private String age;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Builder
+    private Member(final String memberId, final String password, final String name, final String phone,
+                  final String email, final String age, final String gender) {
+        this.memberId = memberId;
+        this.password = password;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.age = age;
+        this.gender = gender;
+    }
 }
