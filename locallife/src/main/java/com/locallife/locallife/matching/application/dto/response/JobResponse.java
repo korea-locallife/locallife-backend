8715f3b150d6e9476d396280dcd471e20aea27dd
JJimini <min102602@pusan.ac.kr>
2024-08-24 19:24:34 +0900

    feat: create 거리 기준 지역별 기업 조회 api

---
+++ locallife/src/main/java/com/locallife/locallife/matching/application/dto/response/JobResponse.java
@@ -0,0 +1,30 @@
+package com.locallife.locallife.matching.application.dto.response;
+
+import com.locallife.locallife.matching.entity.DormitoryJob;
+import com.locallife.locallife.matching.entity.Job;
+import java.util.List;
+import java.util.stream.IntStream;
+import lombok.Builder;
+
+@Builder
+public record JobResponse(
+        String category,
+        String local,
+        String description,
+        Long distance,
+        Long timeByCar,
+        Long timeByTransport
+) {
+    public static List<JobResponse> of(final List<Job> jobs, final List<DormitoryJob> dormitoryJobs) {
+        return IntStream.range(0, jobs.size())
+                .mapToObj(i -> JobResponse.builder()
+                        .category(jobs.get(i).getCategory())
+                        .local(jobs.get(i).getLocal())
+                        .description(jobs.get(i).getDescription())
+                        .distance(dormitoryJobs.get(i).getDistance())
+                        .timeByCar(dormitoryJobs.get(i).getTimeByCar())
+                        .timeByTransport(dormitoryJobs.get(i).getTimeByTransport())
+                        .build())
+                .toList();
+    }
+}
--- locallife/src/main/java/com/locallife/locallife/matching/entity/repository/JobRepository.java
+++ locallife/src/main/java/com/locallife/locallife/matching/entity/repository/JobRepository.java
@@ -1,7 +1,9 @@
 package com.locallife.locallife.matching.entity.repository;
 
 import com.locallife.locallife.matching.entity.Job;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JobRepository extends JpaRepository<Job, Long> {
+    List<Job> findAllByLocal(final String local);
 }
