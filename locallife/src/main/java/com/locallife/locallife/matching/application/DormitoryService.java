9b0236abad91a33d32b5ab6ceadeca193f066c2a
JJimini <min102602@pusan.ac.kr>
2024-08-24 17:29:26 +0900

    feat: create 숙소 상세 조회 api

--- locallife/src/main/java/com/locallife/locallife/matching/application/DormitoryService.java
+++ locallife/src/main/java/com/locallife/locallife/matching/application/DormitoryService.java
@@ -1,5 +1,8 @@
 package com.locallife.locallife.matching.application;
 
+import com.locallife.locallife.matching.entity.Dormitory;
+import com.locallife.locallife.matching.entity.repository.DormitoryRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +11,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class DormitoryService {
+    private final DormitoryRepository dormitoryRepository;
+
+    public List<Dormitory> getDormitoryAll() {
+        return dormitoryRepository.findAll();
+    }
 }
--- locallife/src/main/java/com/locallife/locallife/matching/controller/DormitoryController.java
+++ locallife/src/main/java/com/locallife/locallife/matching/controller/DormitoryController.java
@@ -1,11 +1,25 @@
 package com.locallife.locallife.matching.controller;
 
+import com.locallife.locallife.matching.application.DormitoryService;
+import com.locallife.locallife.matching.entity.Dormitory;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Validated
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/matching")
 public class DormitoryController {
+    private final DormitoryService dormitoryService;
+
+    @GetMapping("/dormitories")
+    public ResponseEntity<List<Dormitory>> getDormitoryAll() {
+        return ResponseEntity.ok(dormitoryService.getDormitoryAll());
+    }
+
 }
